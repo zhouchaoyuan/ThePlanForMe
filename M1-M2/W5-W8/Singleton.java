1f233d9d7ec95844cdf23a9eada05e31d20d5c51
zhouchaoyuan <chaoyuanzhou@foxmail.com>
2015-12-28 21:36:18 +0800

    单例

--- M1-M2/W5-W8/Singleton.java
+++ M1-M2/W5-W8/Singleton.java
@@ -1,23 +1,44 @@
+abstract class Person
+{
+	public abstract void say();
+}
+class Headmaster extends Person//班主任只有一个
+{
+	private Headmaster(){};
+	private static Headmaster headMaster=new Headmaster();
+	public static Headmaster getHeadMaster(){
+		return headMaster;
+	}
+	public void say(){
+		System.out.println(this+" say: I am Headmaster!");
+	}
+}
+class Student extends Person//学生可以有多个人
+{
+	public void say(){
+		System.out.println(this+" say: I am Student!"+this);
+	}
+}
+class Monitor extends Person//可以有一个正班长和多个副班长
+{
+	public void say(){
+		System.out.println(this+" say: I am Monitor!"+this);
+	}
+}
 public class Singleton
 {
-	//私有构造器
-	private Singleton(){};
-	//私有域
-	private static Singleton instance=new Singleton();	
-	//共有类方法
-	public static Singleton getInstance(){
-		return instance;
-	}
-	public void print(){
-		System.out.println("I am SingleTon!");
-	}
 	public static void main(String[] args){
-		Singleton st=getInstance();
-		st.print();
+		Headmaster hm1=Headmaster.getHeadMaster();
+		Headmaster hm2=Headmaster.getHeadMaster();
+		hm1.say();hm2.say();
+		System.out.println("hm1: "+hm1+"; hm2:"+hm2+"; hm1==hm2:"+(hm1==hm2));
+		Student stu1=new Student();
+		Student stu2=new Student();
+		stu1.say();stu2.say();
+		System.out.println("stu1: "+stu1+"; stu2:"+stu2+"; stu1==stu2:"+(stu1==stu2));
+		Monitor mon1=new Monitor();
+		Monitor mon2=new Monitor();
+		mon1.say();mon2.say();
+		System.out.println("mon1: "+mon1+"; mon2:"+mon2+";mon1==mon2:"+(mon1==mon2));
 	}
 }
-/*
-enum SingleTon{
-	INSTANCE;
-}
-*/
